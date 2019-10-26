acde460eabceca78b7e0b3c329e148b86a95e7b0
brendan <Brendan>
2019-10-26 01:15:23 -0700

    test

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, Response, request, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/json")
+def json():
+    return render_template('index.html')
+
+@app.route("/background_process_test")
+def background_process_test():
+    print "Hello"
+    return "nothing"
